cdafaa05df9a50f58cab8aa84d4ebd637a230878
joaquin <joacomorelli1@gmail.com>
2026-08-24 13:45:33 -0300

    prueba de falla en los test

---
+++ src/test/java/com/citypass/movilidad/PipelineFalla.java
@@ -0,0 +1,13 @@
+package com.citypass.movilidad;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class PipelineFailureTest {
+
+    @Test
+    void pipelineDebeFallarSiUnTestFalla() {
+        assertEquals(1, 2);
+    }
+}
